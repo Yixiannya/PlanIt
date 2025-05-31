b8ffe07e7a3145b948db28067cd534d3b1be2aae
Shane <shanetanjun@gmail.com>
2025-05-31 22:22:31 +0800

    User integration complete

--- Data/editEvent.tsx
+++ Data/editEvent.tsx
@@ -1,21 +1,3 @@
 export async function editEvent(id, edit) {
-  try {
-     const editfetch = await fetch(`https://planit-40q0.onrender.com/api/events/${id}`, {
-        method: "PATCH",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(edit),
-     });
 
-     if (!editfetch.ok) {
-        throw new Error(`Cannot edit: ${editfetch.status}`);
-     }
-
-     } catch (error) {
-         console.error('Edit error:', error);
-
-     } finally {
-        console.log('Event edited:', edit);
-     }
 };
