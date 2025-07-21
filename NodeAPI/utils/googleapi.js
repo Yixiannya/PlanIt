9acec617c24da4d0730bce3f87160a39322ffd99
Yixiannya <ohyixian@gmail.com>
2025-07-22 00:54:41 +0800

    Some tweaks to code for auth

--- NodeAPI/utils/googleapi.js
+++ NodeAPI/utils/googleapi.js
@@ -8,4 +8,33 @@ function createOAuth2Client() {
     );
 };
 
-module.exports = { createOAuth2Client };
+async function generateTokens(authCode) {
+    const data = {
+        code: authCode,
+        client_id: process.env.SHANE_WEB_CLIENT_ID,
+        client_secret: process.env.SHANE_WEB_CLIENT_SECRET,
+        redirect_uri: '',
+        grant_type: 'authorization_code',
+    };
+
+
+    const response = await fetch('https://oauth2.googleapis.com/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: params.toString(),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        console.error('Token exchange error:', errorData);
+        throw new Error(`Google token exchange failed: ${errorData.error}`);
+    }
+
+    const tokenData = await response.json();
+    return tokenData;
+
+}
+
+module.exports = { createOAuth2Client, generateTokens };
